24c3d3d01a89080c0738f231ec0274850d2f8fec
Minh Tuan <looongtommy27@gmail.com>
2024-01-15 01:24:51 +0700

    'see-order-history'

---
+++ ECommerceMVC/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace ECommerceMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly Hshop2023Context _context;
+
+        public OrderController(Hshop2023Context context)
+        {
+            _context = context;
+        }
+        public IActionResult History(string id)
+        {
+            var listOrder = _context.HoaDons.Include(i=>i.MaTrangThaiNavigation).Where(p => p.MaKh == id).ToList();
+
+            List<HoaDonVM> listOrderVM = new List<HoaDonVM>();
+            foreach (var hd in listOrder)
+            {
+                var hdVM = new HoaDonVM
+                {
+                    MaHd = hd.MaHd,
+                    NgayDat = hd.NgayDat,
+                    TrangThai = hd.MaTrangThaiNavigation.TenTrangThai,
+
+                };
+                var listHh = _context.ChiTietHds.Include(i=>i.MaHhNavigation).Where(p => p.MaHd == hd.MaHd).ToList();
+                var chiTietHdVM = new List<ChiTietHangHoaVM>();
+                double tongTien = 0;
+                foreach (var hh in listHh)
+                {
+                    ChiTietHangHoaVM chiTietHangHoaVM = new ChiTietHangHoaVM
+                    {
+                        MaHh = hh.MaHh,
+                        TenHh = hh.MaHhNavigation.TenHh,
+                        Hinh = hh.MaHhNavigation.Hinh,
+                        DonGia = hh.DonGia,
+                    };
+                    chiTietHdVM.Add(chiTietHangHoaVM);
+                    tongTien += hh.DonGia;
+                }
+                hdVM.ChiTietHd = chiTietHdVM;
+                hdVM.TongTien = tongTien;
+                listOrderVM.Add(hdVM);
+            }
+            return View(listOrderVM);
+        }
+    }
+}
---
+++ ECommerceMVC/ViewModels/HoaDonVM.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceMVC.Data;
+
+namespace ECommerceMVC.ViewModels
+{
+    public class HoaDonVM
+    {
+        public int MaHd { get; set; }
+        public List<ChiTietHangHoaVM> ChiTietHd { get; set; }
+        public DateTime NgayDat { get; set; }
+        public string TrangThai { get; set; }
+        public double TongTien { get; set; }
+    }
+}
